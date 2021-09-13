52385eb75b71d3a9b08000f59088800e85fb50be
Tum/SI <tum.si@outlook.com>
2021-09-14 01:18:00 +0700

    ready for deployment

---
+++ pages/api/new-meetup.js
@@ -0,0 +1,24 @@
+import { MongoClient } from "mongodb";
+
+// /api/new-meetup
+
+async function handler(req, res) {
+  if (req.method === "POST") {
+    const data = req.body;
+
+    // const { title, image, address, description } = data;
+    const client = await MongoClient.connect(
+      "mongodb+srv://Tum:u6BxODMJhctc1fwj@cluster0.xquwl.mongodb.net/meetups?retryWrites=true&w=majority"
+    );
+    const db = client.db();
+    const meetupsCollection = db.collection("meetups");
+    const result = await meetupsCollection.insertOne(data);
+
+    console.log(result);
+    client.close();
+
+    res.status(201).json({ message: "Meetup inserted!" });
+  }
+}
+
+export default handler;
